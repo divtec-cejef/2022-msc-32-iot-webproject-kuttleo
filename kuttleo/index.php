8425922336c1d70bb3a86c7df37d2db3d67c76c4
Léo Küttel <leo.kuettel@divtec.ch>
2022-04-25 12:40:31 +0200

    dernier commit

--- kuttleo/index.php
+++ kuttleo/index.php
@@ -12,49 +12,53 @@ $MaxMinMeasures = getMaxMinMeasures();
 include VIEW_DIR . 'header.view.php';
 
 ?>
-<body>
-<main>
-    <h1 id="Timer"> </h1>
+    <body>
+<div class="mainbackground">
+    <main>
+        <h1 id="Timer"> </h1>
 
-    <h2  id="Date"><?php  echo date("d/m/Y")?></h2>
 
-    <script>
-        //Fonction permettant d'actualiser le Timer du site
-        setInterval(function() {
-            var currentTime = new Date ( );
-            var currentHours = currentTime.getHours ( );
-            var currentMinutes = currentTime.getMinutes ( );
-            var currentSeconds = currentTime.getSeconds ( );
-            currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-            currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
-            var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
-            currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-            currentHours = ( currentHours == 0 ) ? 12 : currentHours;
-            var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-            document.getElementById("Timer").innerHTML = currentTimeString;
-        }, 1000);
-    </script>
 
-    <img src="res/background.jpg">
+        <script>
+            //Fonction permettant d'actualiser le Timer du site
+            setInterval(function() {
+                var currentTime = new Date ( );
+                var currentHours = currentTime.getHours ( );
+                var currentMinutes = currentTime.getMinutes ( );
+                var currentSeconds = currentTime.getSeconds ( );
+                currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+                currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+                var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
+                currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+                currentHours = ( currentHours == 0 ) ? 12 : currentHours;
+                var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+                document.getElementById("Timer").innerHTML = currentTimeString;
+            }, 1000);
+        </script>
 
-    <div class="Temperature">
-        <h2>Température</h2>
-        <p>Max : <?php echo $MaxMinMeasures["maxTemp"] ?>°C</p>
-        <p>Min : <?php echo $MaxMinMeasures["minTemp"] ?> °C</p>
-        <h3 class="Actuel"><?php echo $lastMeasure["temperature_mes"] ?>°C</h3>
-    </div>
-    <div class="Humidité">
-        <h2>Température</h2>
-        <p>Max : <?php echo $MaxMinMeasures["maxHumi"] ?>%</p>
-        <p>Min : <?php echo $MaxMinMeasures["minHumi"] ?>%</p>
-        <h3 class="Actuel"><?php echo $lastMeasure["humidite_mes"] ?>%</h3>
-    </div>
-    <!-- Flèche -->
-    <span>
+        <!-- <img src="res/background.jpg"> -->
+
+        <div class="Temperature">
+            <h2>Température</h2>
+            <p>Max : <?php echo $MaxMinMeasures["maxTemp"] ?>°C</p>
+            <p>Min : <?php echo $MaxMinMeasures["minTemp"] ?> °C</p>
+            <h3 class="Actuel"><?php echo $lastMeasure["temperature_mes"] ?>°C</h3>
+        </div>
+        <div class="Humidité">
+            <h2>Humidité</h2>
+            <p>Max : <?php echo $MaxMinMeasures["maxHumi"] ?>%</p>
+            <p>Min : <?php echo $MaxMinMeasures["minHumi"] ?>%</p>
+            <h3 class="Actuel"><?php echo $lastMeasure["humidite_mes"] ?>%</h3>
+        </div>
+        <!-- Flèche -->
+        <span>
         <a href="#sectTable">&nbsp;</a>
     </span>
-</main>
 
+        <h2  id="Date"><?php  echo date("d/m/Y")?></h2>
+
+    </main>
+</div>
 <section id="sectTable">
     <div>
         <table>
@@ -74,24 +78,14 @@ include VIEW_DIR . 'header.view.php';
                 <td><?=htmlentities($res['temperature_mes'])?></td>
                 <td><?=htmlentities($res['humidite_mes'])?>%</td>
                 <td>
-                    <button>Modifier</button>
-                    <button>Supprimer</button>
+                    <a>Modifier</a>
+                    <a>Supprimer</a>
                 </td>
             </tr>
             <?php endforeach; ?>
-            <tr>
-                <td>3</td>
-                <td>07-02-2022 14:35</td>
-                <td>21°C</td>
-                <td>30%</td>
-                <td>
-                    <button>Modifier</button>
-                    <button>Supprimer</button>
-                </td>
-            </tr>
         </table>
     </div>
 </section>
 <?php
-    include VIEW_DIR . 'footer.view.php';
+include VIEW_DIR . 'footer.view.php';
 ?>
